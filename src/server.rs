a85c709cfd5bdc8d682bb4f6334028f12da1771b
Lucien Cartier-Tilet <lucien@phundrak.com>
2022-11-24 23:04:16 +0100

    Check correct spelling if original search query bears no results

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,8 @@
 use std::collections::HashMap;
 
+use color_eyre::eyre::Result;
 use diesel::pg::PgConnection;
-use diesel::r2d2::{ConnectionManager, Pool};
+use diesel::r2d2::{ConnectionManager, Pool, PooledConnection};
 use rocket::http::Status;
 use rocket::response::status::Custom;
 use rocket::serde::{json::Json, Deserialize, Serialize};
@@ -12,6 +13,8 @@ use crate::db::{self, models::Document};
 use crate::fileparser::get_content;
 use crate::kwparser;
 use crate::spelling::Dictionary;
+
+type DbPool = PooledConnection<ConnectionManager<PgConnection>>;
 
 #[allow(clippy::module_name_repetitions)]
 pub struct ServerState {
@@ -42,24 +45,30 @@ pub struct RankedKeyword {
 pub struct QueryResult {
     spelling_suggestion: Option<String>,
     results: Vec<RankedDoc>,
+    using_suggestion: bool,
 }
 
 impl QueryResult {
     #[must_use]
     pub fn new(
         results: Vec<RankedDoc>,
-        initial_query: &str,
-        query_suggestion: String,
+        spelling_suggestion: Option<String>,
+        using_suggestion: &UseSpellingSuggestion,
     ) -> Self {
         Self {
-            spelling_suggestion: if initial_query == query_suggestion {
-                None
-            } else {
-                Some(query_suggestion)
-            },
             results,
+            spelling_suggestion,
+            using_suggestion: match using_suggestion {
+                UseSpellingSuggestion::Yes => true,
+                UseSpellingSuggestion::No => false,
+            },
         }
     }
+}
+
+pub enum UseSpellingSuggestion {
+    Yes,
+    No,
 }
 
 macro_rules! api_error {
@@ -89,7 +98,7 @@ macro_rules! json_val_or_error {
     };
 }
 
-pub type ApiResponse<T> = Result<T, Custom<String>>;
+pub type ApiResponse<T> = std::result::Result<T, Custom<String>>;
 
 async fn fetch_content(url: &String) -> ApiResponse<Vec<u8>> {
     match reqwest::get(url).await {
@@ -171,37 +180,96 @@ pub fn delete_document(
 
 // Reading the database ///////////////////////////////////////////////////////
 
+fn search_document_by_keyword(
+    conn: &mut DbPool,
+    query: &[String],
+    spelling_suggestion: &[String],
+    using_suggestion: &UseSpellingSuggestion,
+) -> Result<Json<QueryResult>> {
+    match using_suggestion {
+        // If we are already using the spelling suggestion, return
+        // what we have
+        UseSpellingSuggestion::Yes => {
+            let results = db::keywords_search(conn, spelling_suggestion)?;
+            Ok(Json(QueryResult::new(
+                results,
+                Some(spelling_suggestion.join(" ")),
+                using_suggestion,
+            )))
+        },
+        // If we are not usin the spelling suggestion, only try
+        UseSpellingSuggestion::No => {
+            // If the results are not empty, or if the spelling
+            // suggestion bears no difference with the initial query,
+            // return what we have
+            let results = db::keywords_search(conn, query)?;
+            if !results.is_empty() || query == spelling_suggestion {
+                Ok(Json(QueryResult::new(results, None, using_suggestion)))
+            } else {
+                // Otherwise, if the results were empty and the
+                // initial query is different from the spelling
+                // suggestion, try to search the database using it
+                search_document_by_keyword(
+                    conn,
+                    query,
+                    spelling_suggestion,
+                    &UseSpellingSuggestion::Yes,
+                )
+            }
+        }
+    }
+}
+
 /// Search documents matching the keywords in `query`
+///
+/// This function also executes a spell check on the query. If the
+/// function detects no results are found from the initial query, it
+/// will try to find other results using the spell checked version of
+/// the query. Whether the spell checked version of the query has been
+/// used or not is specified in the [`QueryResult`] type returned.
 ///
 /// # Errors
 ///
 /// Errors might originate from the database, Diesel, or Rocket
+///
+/// [`QueryResult`]: ./struct.QueryResult.html
 #[get("/search/<query>")]
 pub fn search_query(
     query: &str,
     state: &State<ServerState>,
 ) -> ApiResponse<Json<QueryResult>> {
     use crate::spelling::correct;
+    // Filter out empty queries
     info!("Query \"{}\"", query);
     if query.is_empty() {
         return Ok(Json(QueryResult::default()));
     }
+
     let conn = &mut get_connector!(state);
+
+    // Normalize query
     let glaff = &state.glaff;
     let query_vec = query
         .split_whitespace()
         .map(|s| kwparser::get_lemma_from_glaff(s.to_lowercase(), glaff))
         .collect::<Vec<String>>();
-    let query_suggestion = query_vec
+
+    // Spellcheck query
+    debug!("Normalized query_vec: {:?}", query_vec);
+    let spelling_suggestion = query_vec
         .iter()
         .map(|s| correct(s.to_string(), &state.dictionary))
-        .collect::<Vec<String>>()
-        .concat();
-    debug!("Normalized query_vec: {:?}", query_vec);
-    debug!("Suggested query: {}", query_suggestion);
-    db::keywords_search(conn, &query_vec)
-        .map(|results| Json(QueryResult::new(results, query, query_suggestion)))
-        .map_err(|e| Custom(Status::InternalServerError, e.to_string()))
+        .collect::<Vec<String>>();
+
+    // Execute the query
+    debug!("Suggested query: {:?}", spelling_suggestion);
+    search_document_by_keyword(
+        conn,
+        &query_vec,
+        &spelling_suggestion,
+        &UseSpellingSuggestion::No,
+    )
+    .map_err(|e| Custom(Status::InternalServerError, e.to_string()))
 }
 
 /// List indexed documents
